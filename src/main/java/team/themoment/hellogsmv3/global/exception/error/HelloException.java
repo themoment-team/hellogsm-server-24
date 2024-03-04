f63cfcd65340f46ae683652254697d6bc4f05428
신희성 <vvwv3092@gmail.com>
2024-03-04 21:02:56 +0900

    feat HelloException

---
+++ src/main/java/team/themoment/hellogsmv3/global/exception/error/HelloException.java
@@ -0,0 +1,26 @@
+package team.themoment.hellogsmv3.global.exception.error;
+
+import org.springframework.http.HttpStatus;
+
+public class HelloException extends RuntimeException {
+
+    private final HttpStatus statusCode;
+
+    public HttpStatus getStatusCode() {
+        return statusCode;
+    }
+
+    public HelloException(String message, HttpStatus statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+
+    public HelloException(HttpStatus statusCode) {
+        this(statusCode.getReasonPhrase(), statusCode);
+    }
+
+    @Override
+    public synchronized Throwable fillInStackTrace() {
+        return this;
+    }
+}
