6b2c18beaa6e9eb07fa550371a2329200b0968c8
신희성 <vvwv3092@gmail.com>
2024-03-04 21:04:44 +0900

    feat ExceptionResponseEntity

---
+++ src/main/java/team/themoment/hellogsmv3/global/exception/model/ExceptionResponseEntity.java
@@ -0,0 +1,7 @@
+package team.themoment.hellogsmv3.global.exception.model;
+
+public record ExceptionResponseEntity(String message) {
+    public static ExceptionResponseEntity of(final Throwable exception) {
+        return new ExceptionResponseEntity(exception.getMessage());
+    }
+}
