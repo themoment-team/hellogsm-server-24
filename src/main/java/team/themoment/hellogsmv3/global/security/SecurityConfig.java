a3916791850c8426a3aab6ebedb198e6dfa6c990
신희성 <vvwv3092@gmail.com>
2024-04-18 20:25:32 +0900

    add(auth): oauth UserInfo 추가

--- src/main/java/team/themoment/hellogsmv3/global/security/SecurityConfig.java
+++ src/main/java/team/themoment/hellogsmv3/global/security/SecurityConfig.java
@@ -1,9 +1,19 @@
 package team.themoment.hellogsmv3.global.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @RequiredArgsConstructor
@@ -11,6 +21,43 @@ public class SecurityConfig {
     private static final String oauth2LoginEndpointBaseUri = "/auth/v1/oauth2/authorization";
     private static final String oauth2LoginProcessingUri = "/auth/v1/oauth2/code/*";
 
+    @Configuration
+    @EnableWebSecurity
+    public class ProdSecurityConfig {
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+            http
+                    .formLogin().disable()
+                    .httpBasic().disable()
+                    .cors().configurationSource(corsConfigurationSource()).and()
+                    .csrf().disable();
+            oauth2Login(http);
+            authorizeHttpRequests(http);
+            return http.build();
+        }
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    private void oauth2Login(HttpSecurity http) throws Exception {
+        http.oauth2Login(oauth2Login ->
+                oauth2Login
+                        .authorizationEndpoint().baseUri(oauth2LoginEndpointBaseUri).and()
+                        .loginProcessingUrl(oauth2LoginProcessingUri)
+
+        );
+    }
 
     private void authorizeHttpRequests(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(httpRequests -> httpRequests
