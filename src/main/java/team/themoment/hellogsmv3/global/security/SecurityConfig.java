d9e2b763d5f7f961a5302ed69561ef0f54a621dd
신희성 <vvwv3092@gmail.com>
2024-04-19 21:18:26 +0900

    add(auth): 핸들러 추가

--- src/main/java/team/themoment/hellogsmv3/global/security/SecurityConfig.java
+++ src/main/java/team/themoment/hellogsmv3/global/security/SecurityConfig.java
@@ -8,17 +8,24 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import team.themoment.hellogsmv3.domain.auth.type.Role;
+import team.themoment.hellogsmv3.global.security.handler.CustomAccessDeniedHandler;
+import team.themoment.hellogsmv3.global.security.handler.CustomAuthenticationEntryPoint;
+import team.themoment.hellogsmv3.global.security.handler.CustomUrlAuthenticationSuccessHandler;
 
 import java.util.Arrays;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
-    private static final String oauth2LoginEndpointBaseUri = "/auth/v1/oauth2/authorization";
+    private static final String oauth2LoginEndpointBaseUri = "/oauth2/authorization";
     private static final String oauth2LoginProcessingUri = "/login/oauth2/code/*";
+    private final CustomAccessDeniedHandler accessDeniedHandler;
+    private final CustomAuthenticationEntryPoint authenticationEntryPoint;
 
     @Configuration
     @EnableWebSecurity
@@ -32,6 +39,7 @@ public class SecurityConfig {
                     .cors(cors -> cors.configurationSource(corsConfigurationSource()));
 
             oauth2Login(http);
+            exceptionHandling(http);
             authorizeHttpRequests(http);
             return http.build();
         }
@@ -56,15 +64,25 @@ public class SecurityConfig {
                         .authorizationEndpoint(authorizationEndpointConfig ->
                                 authorizationEndpointConfig.baseUri(oauth2LoginEndpointBaseUri))
                         .loginProcessingUrl(oauth2LoginProcessingUri)
+                        .successHandler(new CustomUrlAuthenticationSuccessHandler("http://localhost:8080", "http://localhost:8080/admin"))
+                        .failureHandler(new SimpleUrlAuthenticationFailureHandler("http://localhost:8080/login"))
 
         );
+    }
+
+    private void exceptionHandling(HttpSecurity http) throws Exception {
+        http.exceptionHandling(handling -> handling
+                .accessDeniedHandler(accessDeniedHandler)
+                .authenticationEntryPoint(authenticationEntryPoint));
     }
 
     private void authorizeHttpRequests(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(httpRequests -> httpRequests
                 .requestMatchers(HttpMethod.OPTIONS, "/**/*").permitAll()
                 .requestMatchers("/csrf").permitAll()
-                .requestMatchers("/auth/v1/**").permitAll()
+                .requestMatchers("/auth/v3/**").permitAll()
+                .requestMatchers("/manager").authenticated()
+                .requestMatchers("/admin").hasAnyRole(Role.ADMIN.name())
                 .anyRequest().permitAll()
         );
     }
