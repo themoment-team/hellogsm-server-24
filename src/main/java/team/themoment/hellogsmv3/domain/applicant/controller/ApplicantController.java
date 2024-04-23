c8daba1e6e0ad5e1417bbdfa1f5b0d895e58d0c6
jangwooooo <s22018@gsm.hs.kr>
2024-04-23 09:58:28 +0900

    add(applicant): 컨트롤러 생성

--- src/main/java/team/themoment/hellogsmv3/domain/applicant/controller/ApplicantController.java
+++ src/main/java/team/themoment/hellogsmv3/domain/applicant/controller/ApplicantController.java
@@ -1,2 +1,11 @@
-package team.themoment.hellogsmv3.domain.applicant.controller;public class ApplicantController {
+package team.themoment.hellogsmv3.domain.applicant.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/applicant/v3")
+@RequiredArgsConstructor
+public class ApplicantController {
 }
