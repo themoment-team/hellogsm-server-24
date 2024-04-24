9d923a3012b8fa2f1ea6605beee2bc03520b93f4
신희성 <vvwv3092@gmail.com>
2024-04-24 17:13:43 +0900

    refactor(auth): 불필요한 setAllowedHeaders 삭제

--- src/main/java/team/themoment/hellogsmv3/global/security/SecurityConfig.java
+++ src/main/java/team/themoment/hellogsmv3/global/security/SecurityConfig.java
@@ -62,8 +62,6 @@ public class SecurityConfig {
                 HttpMethod.DELETE.name(),
                 HttpMethod.OPTIONS.name()));
 
-        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
-        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
