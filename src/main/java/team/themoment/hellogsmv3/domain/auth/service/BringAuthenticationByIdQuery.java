4829f058f83793bff4fcdaa7953a51908a2e20c4
hajeu <s22072@gsm.hs.kr>
2024-04-25 19:45:09 +0900

    add(auth): 서비스 메서드에 Transactional readOnly = true 추가

--- src/main/java/team/themoment/hellogsmv3/domain/auth/service/BringAuthenticationByIdQuery.java
+++ src/main/java/team/themoment/hellogsmv3/domain/auth/service/BringAuthenticationByIdQuery.java
@@ -3,6 +3,7 @@ package team.themoment.hellogsmv3.domain.auth.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import team.themoment.hellogsmv3.domain.auth.dto.BasicAuthenticationDto;
 import team.themoment.hellogsmv3.domain.auth.entity.Authentication;
 import team.themoment.hellogsmv3.domain.auth.repo.AuthenticationRepository;
@@ -14,6 +15,7 @@ public class BringAuthenticationByIdQuery {
 
     private final AuthenticationRepository authenticationRepository;
 
+    @Transactional(readOnly = true)
     public BasicAuthenticationDto execute(Long authenticationId) {
         Authentication authentication = authenticationRepository.findById(authenticationId)
                 .orElseThrow(() -> new ExpectedException("authentication을 찾을 수 없습니다.", HttpStatus.NOT_FOUND));
