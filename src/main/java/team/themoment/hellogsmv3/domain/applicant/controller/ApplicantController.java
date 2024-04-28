c8349f823c4e133c6e5560d88a7c6d15e26acddb
jangwooooo <s22018@gsm.hs.kr>
2024-04-28 14:58:23 +0900

    add(applicant): GET /applicant/me api controller

--- src/main/java/team/themoment/hellogsmv3/domain/applicant/controller/ApplicantController.java
+++ src/main/java/team/themoment/hellogsmv3/domain/applicant/controller/ApplicantController.java
@@ -5,13 +5,12 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import team.themoment.hellogsmv3.domain.applicant.dto.request.ApplicantReqDto;
 import team.themoment.hellogsmv3.domain.applicant.dto.response.CreateApplicantResDto;
+import team.themoment.hellogsmv3.domain.applicant.dto.response.FindApplicantResDto;
 import team.themoment.hellogsmv3.domain.applicant.service.CreateApplicantService;
+import team.themoment.hellogsmv3.domain.applicant.service.QueryApplicantService;
 import team.themoment.hellogsmv3.global.security.auth.AuthenticatedUserManager;
 
 import java.util.Map;
@@ -23,6 +22,7 @@ public class ApplicantController {
 
     private final AuthenticatedUserManager manager;
     private final CreateApplicantService createApplicantService;
+    private final QueryApplicantService queryApplicantService;
 
     @PostMapping("/applicant/me")
     public ResponseEntity<Object> create(
@@ -32,5 +32,11 @@ public class ApplicantController {
         CreateApplicantResDto resDto = createApplicantService.execute(reqDto, manager.getId());
         manager.setRole(httpServletRequest, resDto.authenticationRole());
         return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("message", "본인인증이 완료되었습니다"));
+    }
+
+    @GetMapping("/applicant/me")
+    public ResponseEntity<FindApplicantResDto> find() {
+        FindApplicantResDto findApplicantResDto = queryApplicantService.execute(manager.getId());
+        return ResponseEntity.status(HttpStatus.OK).body(findApplicantResDto);
     }
 }
