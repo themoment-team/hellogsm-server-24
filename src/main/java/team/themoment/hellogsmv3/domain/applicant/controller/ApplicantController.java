cb0abbff37ecdc23f935a8eef0b88ebdd050b576
jangwooooo <s22018@gsm.hs.kr>
2024-05-09 11:29:57 +0900

    add(applicant): PUT /applicant/v3/applicant/me api 매핑하는 컨트롤러 메서드 생성

--- src/main/java/team/themoment/hellogsmv3/domain/applicant/controller/ApplicantController.java
+++ src/main/java/team/themoment/hellogsmv3/domain/applicant/controller/ApplicantController.java
@@ -5,16 +5,14 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import team.themoment.hellogsmv3.domain.applicant.dto.request.ApplicantReqDto;
 import team.themoment.hellogsmv3.domain.applicant.dto.request.GenerateCodeReqDto;
 import team.themoment.hellogsmv3.domain.applicant.dto.response.CreateApplicantResDto;
 import team.themoment.hellogsmv3.domain.applicant.service.CreateApplicantService;
 import team.themoment.hellogsmv3.domain.applicant.service.GenerateCodeService;
 import team.themoment.hellogsmv3.domain.applicant.service.GenerateTestCodeService;
+import team.themoment.hellogsmv3.domain.applicant.service.ModifyApplicantService;
 import team.themoment.hellogsmv3.global.security.auth.AuthenticatedUserManager;
 
 import java.util.Map;
@@ -26,6 +24,7 @@ public class ApplicantController {
 
     private final AuthenticatedUserManager manager;
     private final CreateApplicantService createApplicantService;
+    private final ModifyApplicantService modifyApplicantService;
     private final GenerateTestCodeService generateTestCodeService;
     private final GenerateCodeService generateCodeService;
 
@@ -53,5 +52,13 @@ public class ApplicantController {
         CreateApplicantResDto resDto = createApplicantService.execute(reqDto, manager.getId());
         manager.setRole(httpServletRequest, resDto.authenticationRole());
         return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("message", "본인인증이 완료되었습니다"));
+    }
+
+    @PutMapping("/applicant/me")
+    public ResponseEntity<Map<String, String>> modify(
+            @RequestBody @Valid ApplicantReqDto reqDto
+    ) {
+        modifyApplicantService.execute(reqDto, manager.getId());
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of("message", "수정되었습니다"));
     }
 }
