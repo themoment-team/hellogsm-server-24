f55b4cdaeaf2377505e757f79babaa3b55849887
jangwooooo <s22018@gsm.hs.kr>
2024-05-09 09:22:27 +0900

    refactor(applicant): ApplicantReqDto 성별 타입을 String에서 Gender enum클래스로 변경

--- src/main/java/team/themoment/hellogsmv3/domain/applicant/dto/request/ApplicantReqDto.java
+++ src/main/java/team/themoment/hellogsmv3/domain/applicant/dto/request/ApplicantReqDto.java
@@ -3,6 +3,7 @@ package team.themoment.hellogsmv3.domain.applicant.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import team.themoment.hellogsmv3.domain.applicant.type.Gender;
 
 import java.time.LocalDate;
 
@@ -12,8 +13,7 @@ public record ApplicantReqDto(
         @NotBlank String name,
         @Pattern(regexp = "^0(?:\\d|\\d{2})(?:\\d{3}|\\d{4})\\d{4}$")
         @NotBlank String phoneNumber,
-        @Pattern(regexp = "^(MALE|FEMALE)$")
-        @NotBlank String gender,
+        @NotBlank Gender gender,
         @NotBlank LocalDate birth
 ) {
 }
