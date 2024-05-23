b0d49abd9526d3216e7b5d8e25c20acb30428f82
jangwooooo <s22018@gsm.hs.kr>
2024-05-23 11:26:43 +0900

    refactor(applicant): create, modify 서비스에서 중복되는 code validate, delete 작업을 common클래스로 분리

--- src/main/java/team/themoment/hellogsmv3/domain/applicant/service/CreateApplicantService.java
+++ src/main/java/team/themoment/hellogsmv3/domain/applicant/service/CreateApplicantService.java
@@ -8,13 +8,11 @@ import team.themoment.hellogsmv3.domain.applicant.dto.request.ApplicantReqDto;
 import team.themoment.hellogsmv3.domain.applicant.entity.Applicant;
 import team.themoment.hellogsmv3.domain.applicant.entity.AuthenticationCode;
 import team.themoment.hellogsmv3.domain.applicant.repo.ApplicantRepository;
-import team.themoment.hellogsmv3.domain.applicant.repo.CodeRepository;
 import team.themoment.hellogsmv3.domain.auth.entity.Authentication;
 import team.themoment.hellogsmv3.domain.auth.repo.AuthenticationRepository;
 import team.themoment.hellogsmv3.domain.auth.type.Role;
 import team.themoment.hellogsmv3.global.exception.error.ExpectedException;
 
-import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -24,7 +22,7 @@ public class CreateApplicantService {
 
     private final ApplicantRepository applicantRepository;
     private final AuthenticationRepository authenticationRepository;
-    private final CodeRepository codeRepository;
+    private final CommonCodeService commonCodeService;
 
     public Role execute(ApplicantReqDto reqDto, Long authenticationId) {
 
@@ -34,20 +32,7 @@ public class CreateApplicantService {
         if (applicantRepository.existsByAuthenticationId(authenticationId))
             throw new ExpectedException("이미 존재하는 Applicant 입니다", HttpStatus.BAD_REQUEST);
 
-        List<AuthenticationCode> codes = codeRepository.findByAuthenticationId(authenticationId);
-        AuthenticationCode recentCode = codes.stream()
-                .max(Comparator.comparing(AuthenticationCode::getCreatedAt))
-                .orElseThrow(() ->
-                        new ExpectedException("사용자의 code가 존재하지 않습니다. 사용자의 ID : " + authenticationId, HttpStatus.BAD_REQUEST));
-
-        if (!recentCode.getAuthenticated())
-            throw new ExpectedException("유효하지 않은 요청입니다. 인증받지 않은 code입니다.", HttpStatus.BAD_REQUEST);
-
-        if (!recentCode.getCode().equals(reqDto.code()))
-            throw new ExpectedException("유효하지 않은 요청입니다. 이전 혹은 잘못된 형식의 code입니다.", HttpStatus.BAD_REQUEST);
-
-        if (!recentCode.getPhoneNumber().equals(reqDto.phoneNumber()))
-            throw new ExpectedException("유효하지 않은 요청입니다. code인증에 사용되었던 전화번호와 요청에 사용한 전화번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST);
+        List<AuthenticationCode> codes = commonCodeService.validateAndGetRecentCode(authenticationId, reqDto.code(), reqDto.phoneNumber());
 
         Authentication roleUpdatedAuthentication = authenticationRepository.save(
                 new Authentication(
@@ -67,7 +52,7 @@ public class CreateApplicantService {
         );
         applicantRepository.save(newApplicant);
 
-        codes.forEach(code -> codeRepository.deleteById(code.getCode()));
+        commonCodeService.deleteCodes(codes);
 
         return roleUpdatedAuthentication.getRole();
     };
--- src/main/java/team/themoment/hellogsmv3/domain/applicant/service/ModifyApplicantService.java
+++ src/main/java/team/themoment/hellogsmv3/domain/applicant/service/ModifyApplicantService.java
@@ -8,12 +8,9 @@ import team.themoment.hellogsmv3.domain.applicant.dto.request.ApplicantReqDto;
 import team.themoment.hellogsmv3.domain.applicant.entity.Applicant;
 import team.themoment.hellogsmv3.domain.applicant.entity.AuthenticationCode;
 import team.themoment.hellogsmv3.domain.applicant.repo.ApplicantRepository;
-import team.themoment.hellogsmv3.domain.applicant.repo.CodeRepository;
-import team.themoment.hellogsmv3.domain.applicant.type.Gender;
 import team.themoment.hellogsmv3.domain.auth.repo.AuthenticationRepository;
 import team.themoment.hellogsmv3.global.exception.error.ExpectedException;
 
-import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -23,7 +20,7 @@ public class ModifyApplicantService {
 
     private final ApplicantRepository applicantRepository;
     private final AuthenticationRepository authenticationRepository;
-    private final CodeRepository codeRepository;
+    private final CommonCodeService commonCodeService;
 
     public void execute(ApplicantReqDto reqDto, Long authenticationId) {
         if(!authenticationRepository.existsById(authenticationId))
@@ -32,20 +29,7 @@ public class ModifyApplicantService {
         Applicant savedApplicant = applicantRepository.findByAuthenticationId(authenticationId)
                 .orElseThrow(() -> new ExpectedException("존재하지 않는 Applicant 입니다", HttpStatus.BAD_REQUEST));
 
-        List<AuthenticationCode> codes = codeRepository.findByAuthenticationId(authenticationId);
-        AuthenticationCode recentCode = codes.stream()
-                .max(Comparator.comparing(AuthenticationCode::getCreatedAt))
-                .orElseThrow(() ->
-                        new ExpectedException("사용자의 code가 존재하지 않습니다. 사용자의 ID : " + authenticationId, HttpStatus.BAD_REQUEST));
-
-        if (!recentCode.getAuthenticated())
-            throw new ExpectedException("유효하지 않은 요청입니다. 인증받지 않은 code입니다.", HttpStatus.BAD_REQUEST);
-
-        if (!recentCode.getCode().equals(reqDto.code()))
-            throw new ExpectedException("유효하지 않은 요청입니다. 이전 혹은 잘못된 형식의 code입니다.", HttpStatus.BAD_REQUEST);
-
-        if (!recentCode.getPhoneNumber().equals(reqDto.phoneNumber()))
-            throw new ExpectedException("유효하지 않은 요청입니다. code인증에 사용되었던 전화번호와 요청에 사용한 전화번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST);
+        List<AuthenticationCode> codes = commonCodeService.validateAndGetRecentCode(authenticationId, reqDto.code(), reqDto.phoneNumber());
 
         Applicant newApplicant = new Applicant(
                 savedApplicant.getId(),
@@ -57,6 +41,6 @@ public class ModifyApplicantService {
         );
         applicantRepository.save(newApplicant);
 
-        codes.forEach(code -> codeRepository.deleteById(code.getCode()));
+        commonCodeService.deleteCodes(codes);
     }
 }
