4e2d5139ece0f85a09fd9db2863e15edbfa3116f
신희성 <vvwv3092@gmail.com>
2024-06-11 15:40:34 +0900

    add(application): 내 원서 수정 컨트롤러 추가

--- src/main/java/team/themoment/hellogsmv3/domain/application/controller/ApplicationController.java
+++ src/main/java/team/themoment/hellogsmv3/domain/application/controller/ApplicationController.java
@@ -4,12 +4,10 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import team.themoment.hellogsmv3.domain.application.dto.request.ApplicationReqDto;
 import team.themoment.hellogsmv3.domain.application.service.CreateApplicationService;
+import team.themoment.hellogsmv3.domain.application.service.ModifyApplicationService;
 import team.themoment.hellogsmv3.global.security.auth.AuthenticatedUserManager;
 
 import java.util.Map;
@@ -21,11 +19,18 @@ public class ApplicationController {
 
     private final AuthenticatedUserManager manager;
     private final CreateApplicationService createApplicationService;
+    private final ModifyApplicationService modifyApplicationService;
 
     @PostMapping("/application/me")
     public ResponseEntity<Map<String, String>> create(@RequestBody @Valid ApplicationReqDto dto) {
         createApplicationService.execute(dto, manager.getId());
         return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("message", "생성되었습니다"));
+    }
+
+    @PutMapping("/application/me")
+    public ResponseEntity<Map<String, String>> modify(@RequestBody @Valid ApplicationReqDto dto) {
+        modifyApplicationService.execute(dto, manager.getId());
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of("message", "수정되었습니다"));
     }
 
 }
