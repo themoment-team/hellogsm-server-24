b71da5ed1f1e5f6ba1739a4ef8a527dac11a997a
신희성 <vvwv3092@gmail.com>
2024-06-11 20:03:25 +0900

    add(application): 최종 제출 상태 변경 컨트롤러 추가

--- src/main/java/team/themoment/hellogsmv3/domain/application/controller/ApplicationController.java
+++ src/main/java/team/themoment/hellogsmv3/domain/application/controller/ApplicationController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import team.themoment.hellogsmv3.domain.application.dto.request.ApplicationReqDto;
 import team.themoment.hellogsmv3.domain.application.service.CreateApplicationService;
 import team.themoment.hellogsmv3.domain.application.service.ModifyApplicationService;
+import team.themoment.hellogsmv3.domain.application.service.UpdateFinalSubmissionService;
 import team.themoment.hellogsmv3.global.security.auth.AuthenticatedUserManager;
 
 import java.util.Map;
@@ -20,6 +21,7 @@ public class ApplicationController {
     private final AuthenticatedUserManager manager;
     private final CreateApplicationService createApplicationService;
     private final ModifyApplicationService modifyApplicationService;
+    private final UpdateFinalSubmissionService updateFinalSubmissionService;
 
     @PostMapping("/application/me")
     public ResponseEntity<Map<String, String>> create(@RequestBody @Valid ApplicationReqDto dto) {
@@ -38,6 +40,12 @@ public class ApplicationController {
             @RequestBody @Valid ApplicationReqDto body,
             @PathVariable Long userId) {
         modifyApplicationService.execute(body, userId, true);
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of("message", "수정되었습니다"));
+    }
+
+    @PatchMapping("/final-submit")
+    public ResponseEntity<Map<String, String>> finalSubmission() {
+        updateFinalSubmissionService.execute(manager.getId());
         return ResponseEntity.status(HttpStatus.OK).body(Map.of("message", "수정되었습니다"));
     }
 
