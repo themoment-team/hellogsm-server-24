6d0f447b155a1fb3bbb7df5a3354ccf38999e0d2
jangwooooo <s22018@gsm.hs.kr>
2024-06-13 11:49:03 +0900

    add(application): /application/v3/application/{authenticationId} api의 권한 설정

--- src/main/java/team/themoment/hellogsmv3/global/security/SecurityConfig.java
+++ src/main/java/team/themoment/hellogsmv3/global/security/SecurityConfig.java
@@ -153,6 +153,9 @@ public class SecurityConfig {
                         Role.ADMIN.name(),
                         Role.ROOT.name()
                 )
+                .requestMatchers(HttpMethod.GET, "/application/v3/application/{authenticationId}").hasAnyAuthority(
+                        Role.ADMIN.name()
+                )
                 .anyRequest().permitAll()
         );
     }
