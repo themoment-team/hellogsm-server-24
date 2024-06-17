f607e8f9b7d37ceaf839ed950ca9be5c8a76e84b
hajeu <s22072@gsm.hs.kr>
2024-06-17 14:43:46 +0900

    add(thirdParty): /s3/v3/image 이미지 업로드 api 구현

---
+++ src/main/java/team/themoment/hellogsmv3/global/thirdParty/aws/s3/controller/S3Controller.java
@@ -0,0 +1,29 @@
+package team.themoment.hellogsmv3.global.thirdParty.aws.s3.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import team.themoment.hellogsmv3.global.thirdParty.aws.s3.service.UploadImageService;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/s3/v3")
+@RequiredArgsConstructor
+public class S3Controller {
+
+    private final UploadImageService uploadImageService;
+
+    @PostMapping("/image")
+    public ResponseEntity<Map<String, String>> uploadImage(
+            @RequestParam(value = "file") MultipartFile multipartFile
+    ) {
+        String fileUrl = uploadImageService.execute(multipartFile);
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of("fileUrl", fileUrl));
+    }
+}
