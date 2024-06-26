ef676dbb12d99dbdc266fc0e62c7830d8b7216d2
jangwooooo <s22018@gsm.hs.kr>
2024-06-26 17:10:37 +0900

    add(oneseo): EntranceTestResult entity 생성

---
+++ src/main/java/team/themoment/hellogsmv3/domain/oneseo/entity/EntranceTestResult.java
@@ -0,0 +1,39 @@
+package team.themoment.hellogsmv3.domain.oneseo.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import team.themoment.hellogsmv3.domain.application.type.EvaluationStatus;
+import team.themoment.hellogsmv3.domain.application.type.Major;
+
+import java.math.BigDecimal;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class EntranceTestResult {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "entrance _test_result_id")
+    private Long id;
+
+    @Column(name = "first_test_result_score")
+    private BigDecimal firstTestResultScore;
+
+    @Column(name = "first_test_pass_yn")
+    private EvaluationStatus firstTestPassYn;
+
+    @Column(name = "second_test_result_score")
+    private BigDecimal secondTestResultScore;
+
+    @Column(name = "second_test_pass_yn")
+    private EvaluationStatus secondTestPassYn;
+
+    @Column(name = "interview_score")
+    private BigDecimal interviewScore;
+
+    @Column(name = "decided_major")
+    private Major decidedMajor;
+}
