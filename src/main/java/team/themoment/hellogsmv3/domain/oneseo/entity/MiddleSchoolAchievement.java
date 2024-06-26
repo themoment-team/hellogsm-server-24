87342f7bb4b206a205187ec74e29d367a3780790
jangwooooo <s22018@gsm.hs.kr>
2024-06-26 15:36:49 +0900

    add(oneseo): MiddleSchoolAchievement entity 생성

---
+++ src/main/java/team/themoment/hellogsmv3/domain/oneseo/entity/MiddleSchoolAchievement.java
@@ -0,0 +1,68 @@
+package team.themoment.hellogsmv3.domain.oneseo.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MiddleSchoolAchievement {
+
+    @Id
+    @Column(name = "oneseo_id")
+    private Long id;
+
+//    @MapsId
+//    @OneToOne
+//    @JoinColumn(name = "id")  TODO 원서 테이블 생성 후 주석 풀기
+//    private Oneseo oneseo;
+
+    @Column(name = "transcript", columnDefinition = "TEXT", nullable = false)
+    private String transcript;
+
+    @Column(name = "percentile_rank", nullable = false)
+    private BigDecimal percentileRank;
+
+    @Column(name = "total_score", nullable = false)
+    private BigDecimal totalScore;
+
+    @Column(name = "artistic_score")
+    private BigDecimal artisticScore;
+
+    @Column(name = "attendance_score")
+    private BigDecimal attendanceScore;
+
+    @Column(name = "volunteer_score")
+    private BigDecimal volunteerScore;
+
+    @Column(name = "curricular_subtotal_score")
+    private BigDecimal curricularSubtotalScore;
+
+    @Column(name = "extra_curricular_subtotal_score")
+    private BigDecimal extraCurricularSubtotalScore;
+
+    @Column(name = "ged_max_score")
+    private BigDecimal gedMaxScore;
+
+    @Column(name = "ged_total_score")
+    private BigDecimal gedTotalScore;
+
+    @Column(name = "grade1_semester1_score")
+    private BigDecimal grade1Semester1Score;
+
+    @Column(name = "grade1_semester2_score")
+    private BigDecimal grade1Semester2Score;
+
+    @Column(name = "grade2_semester1_score")
+    private BigDecimal grade2Semester1Score;
+
+    @Column(name = "grade2_semester2_score")
+    private BigDecimal grade2Semester2Score;
+
+    @Column(name = "grade3_semester1_score")
+    private BigDecimal grade3Semester1Score;
+}
