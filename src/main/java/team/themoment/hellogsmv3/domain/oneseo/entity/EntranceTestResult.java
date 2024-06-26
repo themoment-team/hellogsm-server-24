5c67632af49852d5d0639691dd9ae27a5f97d24c
jangwooooo <s22018@gsm.hs.kr>
2024-06-26 17:13:40 +0900

    add(oneseo): firstTestPassYn, secondTestPassYn, decidedMajor enum class를 string으로 저장하도록 설정

--- src/main/java/team/themoment/hellogsmv3/domain/oneseo/entity/EntranceTestResult.java
+++ src/main/java/team/themoment/hellogsmv3/domain/oneseo/entity/EntranceTestResult.java
@@ -22,18 +22,21 @@ public class EntranceTestResult {
     @Column(name = "first_test_result_score")
     private BigDecimal firstTestResultScore;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "first_test_pass_yn")
     private EvaluationStatus firstTestPassYn;
 
     @Column(name = "second_test_result_score")
     private BigDecimal secondTestResultScore;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "second_test_pass_yn")
     private EvaluationStatus secondTestPassYn;
 
     @Column(name = "interview_score")
     private BigDecimal interviewScore;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "decided_major")
     private Major decidedMajor;
 }
