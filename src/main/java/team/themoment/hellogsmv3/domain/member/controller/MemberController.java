46c098845126aef4c562accf29a5adc88f03b627
jangwooooo <s22018@gsm.hs.kr>
2024-07-02 14:50:37 +0900

    add(member): MemberController 생성 & /member/v3/member/me api 매핑

---
+++ src/main/java/team/themoment/hellogsmv3/domain/member/controller/MemberController.java
@@ -0,0 +1,20 @@
+package team.themoment.hellogsmv3.domain.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import team.themoment.hellogsmv3.domain.member.dto.FoundMemberResDto;
+import team.themoment.hellogsmv3.domain.member.service.QueryMemberByIdService;
+import team.themoment.hellogsmv3.global.common.handler.annotation.AuthRequest;
+
+@RestController
+@RequestMapping("/member/v3")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final QueryMemberByIdService queryMemberByIdService;
+
+    @GetMapping("/member/me")
+    public FoundMemberResDto find(@AuthRequest Long memberId) {
+        return queryMemberByIdService.execute(memberId);
+    }
+}
