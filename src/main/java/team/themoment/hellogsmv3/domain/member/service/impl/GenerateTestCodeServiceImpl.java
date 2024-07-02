9083ad51d5daf7c1868d97df9a833f36f5ce4222
신희성 <vvwv3092@gmail.com>
2024-07-02 17:18:42 +0900

    add(member): 인증코드 생성로직 추가

---
+++ src/main/java/team/themoment/hellogsmv3/domain/member/service/impl/GenerateTestCodeServiceImpl.java
@@ -0,0 +1,38 @@
+package team.themoment.hellogsmv3.domain.member.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import team.themoment.hellogsmv3.domain.applicant.dto.request.GenerateCodeReqDto;
+import team.themoment.hellogsmv3.domain.applicant.entity.AuthenticationCode;
+import team.themoment.hellogsmv3.domain.applicant.repo.CodeRepository;
+import team.themoment.hellogsmv3.domain.applicant.service.GenerateCodeService;
+
+import java.time.LocalDateTime;
+import java.util.Random;
+
+@Service
+@RequiredArgsConstructor
+public class GenerateTestCodeServiceImpl extends GenerateCodeService {
+
+    private final CodeRepository codeRepository;
+    private static final Random RANDOM = new Random();
+
+    @Override
+    public String execute(Long authenticationId, GenerateCodeReqDto reqDto) {
+        final String code = generateUniqueCode(RANDOM, codeRepository);
+
+        AuthenticationCode authenticationCode = codeRepository.findByAuthenticationId(authenticationId)
+                .orElse(null);
+
+        codeRepository.save(createAuthenticationCode(
+                authenticationCode,
+                authenticationId,
+                code,
+                reqDto.phoneNumber(),
+                true));
+
+        codeRepository.save(new AuthenticationCode(authenticationId, code, reqDto.phoneNumber(), LocalDateTime.now()));
+
+        return code;
+    }
+}
