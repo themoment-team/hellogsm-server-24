c986b03bec556eb7ecc1a67a97d714b0a61c6f0b
jangwooooo <s22018@gsm.hs.kr>
2024-07-04 19:59:51 +0900

    add(member): 지원자 정보 수정 기능 구현 ModifyMemberService

---
+++ src/main/java/team/themoment/hellogsmv3/domain/member/service/ModifyMemberService.java
@@ -0,0 +1,41 @@
+package team.themoment.hellogsmv3.domain.member.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import team.themoment.hellogsmv3.domain.applicant.entity.ApplicantAuthenticationCode;
+import team.themoment.hellogsmv3.domain.applicant.service.CommonCodeService;
+import team.themoment.hellogsmv3.domain.member.dto.request.ModifyMemberReqDto;
+import team.themoment.hellogsmv3.domain.member.entity.Member;
+import team.themoment.hellogsmv3.domain.member.repo.MemberRepository;
+import team.themoment.hellogsmv3.global.exception.error.ExpectedException;
+
+@Service
+@RequiredArgsConstructor
+public class ModifyMemberService {
+
+    private final MemberRepository memberRepository;
+    private final CommonCodeService commonCodeService;
+
+    public void execute(ModifyMemberReqDto reqDto, Long memberId) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new ExpectedException("존재하지 않는 지원자입니다. member ID: " + memberId, HttpStatus.NOT_FOUND));
+
+        ApplicantAuthenticationCode code = commonCodeService.validateAndGetRecentCode(memberId, reqDto.code(), reqDto.phoneNumber());
+
+        Member newMember = Member.builder()
+                .id(member.getId())
+                .email(member.getEmail())
+                .authReferrerType(member.getAuthReferrerType())
+                .name(reqDto.name())
+                .birth(reqDto.birth())
+                .phoneNumber(reqDto.phoneNumber())
+                .sex(reqDto.sex())
+                .role(member.getRole())
+                .build();
+
+        memberRepository.save(newMember);
+
+        commonCodeService.deleteCode(code);
+    }
+}
