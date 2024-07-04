fabfeb93660560d8b071b8cdb7598232abcd6bdf
jangwooooo <s22018@gsm.hs.kr>
2024-07-04 12:07:07 +0900

    add(member): 새로 생성한 AuthenticationCode를 사용하는 CommonCodeService생성

---
+++ src/main/java/team/themoment/hellogsmv3/domain/member/service/CommonCodeService.java
@@ -0,0 +1,38 @@
+package team.themoment.hellogsmv3.domain.member.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import team.themoment.hellogsmv3.domain.member.entity.AuthenticationCode;
+import team.themoment.hellogsmv3.domain.member.repo.CodeRepository;
+import team.themoment.hellogsmv3.global.exception.error.ExpectedException;
+
+@Service
+@RequiredArgsConstructor
+public class CommonCodeService {
+
+    private final CodeRepository codeRepository;
+
+    public AuthenticationCode validateAndGetRecentCode(Long memberId, String inputCode, String inputPhoneNumber) {
+        AuthenticationCode code = codeRepository.findByMemberId(memberId)
+                .orElseThrow(() -> new ExpectedException("사용자의 code가 존재하지 않습니다. 사용자의 ID : " + memberId, HttpStatus.BAD_REQUEST));
+
+        if (!code.getAuthenticated()) {
+            throw new ExpectedException("유효하지 않은 요청입니다. 인증받지 않은 code입니다.", HttpStatus.BAD_REQUEST);
+        }
+
+        if (!code.getCode().equals(inputCode)) {
+            throw new ExpectedException("유효하지 않은 요청입니다. 이전 혹은 잘못된 형식의 code입니다.", HttpStatus.BAD_REQUEST);
+        }
+
+        if (!code.getPhoneNumber().equals(inputPhoneNumber)) {  // TODO 반환 메시지에 전화번호 추가?
+            throw new ExpectedException("유효하지 않은 요청입니다. code인증에 사용되었던 전화번호와 요청에 사용한 전화번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST);
+        }
+
+        return code;
+    }
+
+    public void deleteCode(AuthenticationCode code) {      // TODO recentCode만 지워도 되는지 check
+        codeRepository.delete(code);
+    }
+}
