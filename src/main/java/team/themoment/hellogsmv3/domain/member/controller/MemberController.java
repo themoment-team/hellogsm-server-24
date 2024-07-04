c7db52efa6f3f4f7e45a18cbdc5230d5342defb7
jangwooooo <s22018@gsm.hs.kr>
2024-07-04 11:52:08 +0900

    add(member): 지원자 본인인증 api인 POST /member/v3/member/me api와 매핑하는 컨트롤러 메서드 생성

--- src/main/java/team/themoment/hellogsmv3/domain/member/controller/MemberController.java
+++ src/main/java/team/themoment/hellogsmv3/domain/member/controller/MemberController.java
@@ -1,17 +1,26 @@
 package team.themoment.hellogsmv3.domain.member.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import team.themoment.hellogsmv3.domain.member.dto.CreateMemberReqDto;
 import team.themoment.hellogsmv3.domain.member.dto.FoundMemberResDto;
+import team.themoment.hellogsmv3.domain.member.entity.type.Role;
+import team.themoment.hellogsmv3.domain.member.service.CreateMemberService;
 import team.themoment.hellogsmv3.domain.member.service.QueryMemberByIdService;
 import team.themoment.hellogsmv3.global.common.handler.annotation.AuthRequest;
+import team.themoment.hellogsmv3.global.common.response.CommonApiResponse;
+import team.themoment.hellogsmv3.global.security.auth.AuthenticatedUserManager;
 
 @RestController
 @RequestMapping("/member/v3")
 @RequiredArgsConstructor
 public class MemberController {
 
+    private final AuthenticatedUserManager manager;
     private final QueryMemberByIdService queryMemberByIdService;
+    private final CreateMemberService createMemberService;
 
     @GetMapping("/member/me")
     public FoundMemberResDto find(
@@ -25,5 +34,16 @@ public class MemberController {
             @PathVariable Long memberId
     ) {
         return queryMemberByIdService.execute(memberId);
+    }
+
+    @PostMapping("/member/me")
+    public CommonApiResponse create(
+            HttpServletRequest httpServletRequest,
+            @RequestBody @Valid CreateMemberReqDto reqDto,
+            @AuthRequest Long memberId
+    ) {
+        Role role = createMemberService.execute(reqDto, memberId);
+        manager.setRole(httpServletRequest, role);
+        return CommonApiResponse.created("본인인증이 완료되었습니다.");
     }
 }
