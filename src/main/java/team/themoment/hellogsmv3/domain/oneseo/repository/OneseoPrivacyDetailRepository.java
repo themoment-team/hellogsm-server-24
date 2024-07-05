185b707cacab9ec75bd2faaaf93ccd8ea7226d5f
신희성 <vvwv3092@gmail.com>
2024-07-05 14:27:24 +0900

    add(oneseo): 원서 식별관계 엔티티 리포지토리 추가

---
+++ src/main/java/team/themoment/hellogsmv3/domain/oneseo/repository/OneseoPrivacyDetailRepository.java
@@ -0,0 +1,6 @@
+package team.themoment.hellogsmv3.domain.oneseo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OneseoPrivacyDetailRepository extends JpaRepository<OneseoPrivacyDetailRepository, Long> {
+}
---
+++ src/main/java/team/themoment/hellogsmv3/domain/oneseo/repository/OneseoRepository.java
@@ -0,0 +1,7 @@
+package team.themoment.hellogsmv3.domain.oneseo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import team.themoment.hellogsmv3.domain.oneseo.entity.Oneseo;
+
+public interface OneseoRepository extends JpaRepository<Oneseo, Long> {
+}
