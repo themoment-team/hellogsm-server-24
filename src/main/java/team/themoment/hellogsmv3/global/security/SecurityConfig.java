58c45aeb00a57ebf3f998ddb9a75a7ea11cb76f5
신희성 <vvwv3092@gmail.com>
2024-07-12 10:16:03 +0900

    add(oneseo): 원서 최종제출 API 시큐리티 권한 매핑 추가

--- src/main/java/team/themoment/hellogsmv3/global/security/SecurityConfig.java
+++ src/main/java/team/themoment/hellogsmv3/global/security/SecurityConfig.java
@@ -222,6 +222,9 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.PUT, "/oneseo/v3/oneseo/{memberId}").hasAnyAuthority(
                         Role.ADMIN.name()
                 )
+                .requestMatchers(HttpMethod.PUT, "/oneseo/v3/final-submit").hasAnyAuthority(
+                        Role.APPLICANT.name()
+                )
 
                 .anyRequest().permitAll()
         );
