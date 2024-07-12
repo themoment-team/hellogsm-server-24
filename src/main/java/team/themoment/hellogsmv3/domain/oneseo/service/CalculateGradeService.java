fd0e1f4834f23bbdd223f337ce3330829f40592b
신희성 <vvwv3092@gmail.com>
2024-07-12 14:15:08 +0900

    add(oneseo): 봉사성적 환산 로직 추가

--- src/main/java/team/themoment/hellogsmv3/domain/oneseo/service/CalculateGradeService.java
+++ src/main/java/team/themoment/hellogsmv3/domain/oneseo/service/CalculateGradeService.java
@@ -84,25 +84,28 @@ public class CalculateGradeService {
         BigDecimal attendanceScore = calcAttendanceScore(
                 middleSchoolAchievement.getAbsentDays(), middleSchoolAchievement.getAttendanceDays());
 
+        // 봉사 성적 (총점: 30점)
+        BigDecimal volunteerScore = calcVolunteerScore(middleSchoolAchievement.getVolunteerTime());
+
     }
 
-    private BigDecimal calcGeneralScore(List<BigDecimal> scoreArray, BigDecimal maxPoint) {
+    private BigDecimal calcGeneralScore(List<BigDecimal> achievements, BigDecimal maxPoint) {
         // 해당 학기의 등급별 점수 배열이 비어있거나 해당 학기의 배점이 없다면 0.000을 반환
-        if (scoreArray == null || scoreArray.isEmpty() || maxPoint.equals(BigDecimal.ZERO)) return BigDecimal.valueOf(0).setScale(3, RoundingMode.HALF_UP);
+        if (achievements == null || achievements.isEmpty() || maxPoint.equals(BigDecimal.ZERO)) return BigDecimal.valueOf(0).setScale(3, RoundingMode.HALF_UP);
         // 해당 학기에 수강하지 않은 과목이 있다면 제거한 리스트를 반환 (점수가 0인 원소 제거)
-        List<BigDecimal> noZeroScoreArray = scoreArray.stream().filter((score) -> score.compareTo(BigDecimal.ZERO) != 0).toList();
+        List<BigDecimal> noZeroAchievements = achievements.stream().filter((score) -> score.compareTo(BigDecimal.ZERO) != 0).toList();
         // 위에서 구한 리스트가 비어있다면 0.000을 반환
-        if (noZeroScoreArray.isEmpty()) return BigDecimal.valueOf(0).setScale(3, RoundingMode.HALF_UP);
+        if (noZeroAchievements.isEmpty()) return BigDecimal.valueOf(0).setScale(3, RoundingMode.HALF_UP);
 
         // 1. 점수로 환산된 등급을 모두 더한다.
-        BigDecimal reduceResult = scoreArray.stream().reduce(BigDecimal.ZERO, BigDecimal::add);
+        BigDecimal reduceResult = achievements.stream().reduce(BigDecimal.ZERO, BigDecimal::add);
         // 2. 더한값 / (과목 수 * 5) (소수점 6째자리에서 반올림)
-        BigDecimal divideResult = reduceResult.divide(BigDecimal.valueOf(noZeroScoreArray.size() * 5L), 5, RoundingMode.HALF_UP);
+        BigDecimal divideResult = reduceResult.divide(BigDecimal.valueOf(noZeroAchievements.size() * 5L), 5, RoundingMode.HALF_UP);
         // 3. 각 학기당 배점 * 나눈값 (소수점 4째자리에서 반올림)
         return divideResult.multiply(maxPoint).setScale(3, RoundingMode.HALF_UP);
     }
 
-    private BigDecimal calcArtSportsScore(List<BigDecimal> scoreArray) {
+    private BigDecimal calcArtSportsScore(List<BigDecimal> achievements) {
         int aCount = 0;
         int bCount = 0;
         int cCount = 0;
@@ -111,7 +114,7 @@ public class CalculateGradeService {
         BigDecimal B = BigDecimal.valueOf(4);
         BigDecimal C = BigDecimal.valueOf(3);
 
-        for (BigDecimal score : scoreArray) {
+        for (BigDecimal score : achievements) {
             if (score.equals(A)) {
                 aCount++;
             } else if (score.equals(B)) {
@@ -139,23 +142,48 @@ public class CalculateGradeService {
         return BigDecimal.valueOf(60).multiply(averageOfAchievementScale).setScale(3, RoundingMode.HALF_UP);
     }
 
-    private BigDecimal calcAttendanceScore(List<BigDecimal> absentScore, List<BigDecimal> attendanceScore) {
+    private BigDecimal calcAttendanceScore(List<BigDecimal> absentDays, List<BigDecimal> attendanceDays) {
         // 결석 횟수를 더함
-        BigDecimal absent = absentScore.stream().reduce(BigDecimal.ZERO, BigDecimal::add);
+        BigDecimal absentDay = absentDays.stream().reduce(BigDecimal.ZERO, BigDecimal::add);
         // 결석 횟수가 10회 이상 0점을 반환
-        if (absent.compareTo(BigDecimal.TEN) >= 0) return BigDecimal.valueOf(0);
+        if (absentDay.compareTo(BigDecimal.TEN) >= 0) return BigDecimal.valueOf(0);
 
         // 1. 모든 지각, 조퇴, 결과 횟수를 더함
-        BigDecimal attendance = attendanceScore.stream().reduce(BigDecimal.ZERO, BigDecimal::add);
+        BigDecimal attendanceDay = attendanceDays.stream().reduce(BigDecimal.ZERO, BigDecimal::add);
         // 2. 지각, 조퇴, 결과 횟수는 3개당 결석 1회
-        BigDecimal absentResult = attendance.divide(BigDecimal.valueOf(3), 0, RoundingMode.DOWN);
+        BigDecimal absentResult = attendanceDay.divide(BigDecimal.valueOf(3), 0, RoundingMode.DOWN);
         // 3. 총점(30점) - (3 * 총 결석 횟수)
-        BigDecimal result = BigDecimal.valueOf(30).subtract(absent.add(absentResult).multiply(BigDecimal.valueOf(3)));
+        BigDecimal result = BigDecimal.valueOf(30).subtract(absentDay.add(absentResult).multiply(BigDecimal.valueOf(3)));
 
         // 총 점수가 0점 이하라면 0점을 반환
         if (result.compareTo(BigDecimal.ZERO) <= 0) return BigDecimal.ZERO;
 
         return result;
+    }
+
+    private BigDecimal calcVolunteerScore(List<BigDecimal> volunteerHour) {
+        return volunteerHour.stream().reduce(BigDecimal.valueOf(0), (current, hour) -> {
+            // 연간 7시간 이상
+            if (hour.compareTo(BigDecimal.valueOf(7)) >= 0) {
+                return current.add(BigDecimal.valueOf(10));
+            }
+            // 연간 6시간 이상
+            else if (hour.compareTo(BigDecimal.valueOf(6)) >= 0) {
+                return current.add(BigDecimal.valueOf(8));
+            }
+            // 연간 5시간 이상
+            else if (hour.compareTo(BigDecimal.valueOf(5)) >= 0) {
+                return current.add(BigDecimal.valueOf(6));
+            }
+            // 연간 4시간 이상
+            else if (hour.compareTo(BigDecimal.valueOf(4)) >= 0) {
+                return current.add(BigDecimal.valueOf(4));
+            }
+            // 연간 3시간 이하
+            else {
+                return current.add(BigDecimal.valueOf(2));
+            }
+        });
     }
 
 }
