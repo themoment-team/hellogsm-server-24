ab20ac6cab7f8f22cd4bd10eac3e3d5eac67607f
신희성 <vvwv3092@gmail.com>
2024-07-12 14:02:47 +0900

    add(oneseo): 출결 성적 환산 로직 추가

--- src/main/java/team/themoment/hellogsmv3/domain/oneseo/service/CalculateGradeService.java
+++ src/main/java/team/themoment/hellogsmv3/domain/oneseo/service/CalculateGradeService.java
@@ -63,7 +63,7 @@ public class CalculateGradeService {
                         liberalSystem.equals("3-1") ? 0 : 72)
         );
 
-        // 교과 성적 환산값 (총점: 180점)
+        // 일반 교과 성적 환산값 (총점: 180점)
         BigDecimal generalSubjectsScore = Stream.of(
                         achievement1_2,
                         achievement2_1,
@@ -73,6 +73,16 @@ public class CalculateGradeService {
 
         // 예체능 성적 환산값 (총점: 60점)
         BigDecimal artsPhysicalSubjectsScore = calcArtSportsScore(middleSchoolAchievement.getArtsPhysicalAchievement());
+
+
+        // 교과 성적 환산값 (예체능 성적 + 일반 교과 성적, 총점: 240점)
+        BigDecimal totalSubjectsScore = artsPhysicalSubjectsScore
+                .add(generalSubjectsScore)
+                .setScale(3, RoundingMode.HALF_UP);
+
+        // 출결 성적 (총점: 30점)
+        BigDecimal attendanceScore = calcAttendanceScore(
+                middleSchoolAchievement.getAbsentDays(), middleSchoolAchievement.getAttendanceDays());
 
     }
 
@@ -111,11 +121,11 @@ public class CalculateGradeService {
             }
         }
 
-        // 각 등급별 갯수에 등급별 배점을 곱한 값을 더한다.
+        // 1. 각 등급별 갯수에 등급별 배점을 곱한 값을 더한다.
         int totalScores = (aCount * 5) + (bCount * 4) + (cCount * 3);
-        // 각 등급별 갯수를 모두 더해 과목 수를 구한다.
+        // 2. 각 등급별 갯수를 모두 더해 과목 수를 구한다.
         int totalSubjects = aCount + bCount + cCount;
-        // 각 등급별 갯수를 더한 값에 5를 곰해 총점을 구한다.
+        // 3. 각 등급별 갯수를 더한 값에 5를 곰해 총점을 구한다.
         int maxScore = 5 * totalSubjects;
 
         // TODO 작년 구현에서 과목수가 0일시 36.00점을 반환하는 로직의 존재 이유를 모르겠어서 주석처리
@@ -127,6 +137,25 @@ public class CalculateGradeService {
 
         BigDecimal averageOfAchievementScale  = BigDecimal.valueOf(totalScores).divide(BigDecimal.valueOf(maxScore), 3, RoundingMode.HALF_UP);
         return BigDecimal.valueOf(60).multiply(averageOfAchievementScale).setScale(3, RoundingMode.HALF_UP);
+    }
+
+    private BigDecimal calcAttendanceScore(List<BigDecimal> absentScore, List<BigDecimal> attendanceScore) {
+        // 결석 횟수를 더함
+        BigDecimal absent = absentScore.stream().reduce(BigDecimal.ZERO, BigDecimal::add);
+        // 결석 횟수가 10회 이상 0점을 반환
+        if (absent.compareTo(BigDecimal.TEN) >= 0) return BigDecimal.valueOf(0);
+
+        // 1. 모든 지각, 조퇴, 결과 횟수를 더함
+        BigDecimal attendance = attendanceScore.stream().reduce(BigDecimal.ZERO, BigDecimal::add);
+        // 2. 지각, 조퇴, 결과 횟수는 3개당 결석 1회
+        BigDecimal absentResult = attendance.divide(BigDecimal.valueOf(3), 0, RoundingMode.DOWN);
+        // 3. 총점(30점) - (3 * 총 결석 횟수)
+        BigDecimal result = BigDecimal.valueOf(30).subtract(absent.add(absentResult).multiply(BigDecimal.valueOf(3)));
+
+        // 총 점수가 0점 이하라면 0점을 반환
+        if (result.compareTo(BigDecimal.ZERO) <= 0) return BigDecimal.ZERO;
+
+        return result;
     }
 
 }
