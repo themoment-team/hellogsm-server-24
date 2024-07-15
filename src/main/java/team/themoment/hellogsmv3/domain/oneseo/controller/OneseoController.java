8e061fe00484895e32d14cf8edf90f8569914fab
신희성 <vvwv3092@gmail.com>
2024-07-15 10:03:57 +0900

    add(oneseo): 모의성적 계산 컨트롤러 추가

--- src/main/java/team/themoment/hellogsmv3/domain/oneseo/controller/OneseoController.java
+++ src/main/java/team/themoment/hellogsmv3/domain/oneseo/controller/OneseoController.java
@@ -3,7 +3,11 @@ package team.themoment.hellogsmv3.domain.oneseo.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import team.themoment.hellogsmv3.domain.oneseo.dto.request.MiddleSchoolAchievementReqDto;
 import team.themoment.hellogsmv3.domain.oneseo.dto.request.OneseoReqDto;
+import team.themoment.hellogsmv3.domain.oneseo.dto.response.MockScoreResDto;
+import team.themoment.hellogsmv3.domain.oneseo.entity.type.GraduationType;
+import team.themoment.hellogsmv3.domain.oneseo.service.CalculateMockScoreService;
 import team.themoment.hellogsmv3.domain.oneseo.service.CreateOneseoService;
 import team.themoment.hellogsmv3.domain.oneseo.service.ModifyOneseoService;
 import team.themoment.hellogsmv3.domain.oneseo.service.UpdateFinalSubmissionService;
@@ -18,6 +22,7 @@ public class OneseoController {
     private final CreateOneseoService createOneseoService;
     private final ModifyOneseoService modifyOneseoService;
     private final UpdateFinalSubmissionService updateFinalSubmissionService;
+    private final CalculateMockScoreService calculateMockScoreService;
 
     @PostMapping("/oneseo/me")
     public CommonApiResponse create(
@@ -52,6 +57,14 @@ public class OneseoController {
     ) {
         updateFinalSubmissionService.execute(memberId);
         return CommonApiResponse.success("수정되었습니다.");
+    }
+
+    @PostMapping("/calculate-mock-score")
+    public MockScoreResDto calcMockScore(
+            @RequestBody MiddleSchoolAchievementReqDto dto,
+            @RequestParam("graduationType") GraduationType graduationType
+        ) {
+        return calculateMockScoreService.execute(dto, graduationType);
     }
 
 }
