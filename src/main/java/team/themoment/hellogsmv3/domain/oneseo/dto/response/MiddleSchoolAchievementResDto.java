812cb76b0e80eb9e4ebb4eb7390c31d6f0bdf970
jangwooooo <s22018@gsm.hs.kr>
2024-07-17 10:02:04 +0900

    update(oneseo): 변경된 도메인에 맞게 res dto 구조 변경

--- src/main/java/team/themoment/hellogsmv3/domain/oneseo/dto/response/MiddleSchoolAchievementResDto.java
+++ src/main/java/team/themoment/hellogsmv3/domain/oneseo/dto/response/MiddleSchoolAchievementResDto.java
@@ -8,11 +8,11 @@ import java.util.List;
 @Builder
 public record MiddleSchoolAchievementResDto(
 
-        List<Integer> achievement1_1,
         List<Integer> achievement1_2,
         List<Integer> achievement2_1,
         List<Integer> achievement2_2,
         List<Integer> achievement3_1,
+        List<Integer> achievement3_2,
         List<String> generalSubjects,
         List<String> newSubjects,
         List<Integer> artsPhysicalAchievement,
