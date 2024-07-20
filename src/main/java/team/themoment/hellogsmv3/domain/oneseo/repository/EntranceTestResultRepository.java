d38540510854bfb991778da7a472883d291e2209
jangwooooo <s22018@gsm.hs.kr>
2024-07-20 11:01:25 +0900

    conflict resolve

--- src/main/java/team/themoment/hellogsmv3/domain/oneseo/repository/EntranceTestResultRepository.java
+++ src/main/java/team/themoment/hellogsmv3/domain/oneseo/repository/EntranceTestResultRepository.java
@@ -2,7 +2,6 @@ package team.themoment.hellogsmv3.domain.oneseo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.themoment.hellogsmv3.domain.oneseo.entity.EntranceTestResult;
-<<<<<<< HEAD
 import team.themoment.hellogsmv3.domain.oneseo.entity.Oneseo;
 
 import java.util.Optional;
