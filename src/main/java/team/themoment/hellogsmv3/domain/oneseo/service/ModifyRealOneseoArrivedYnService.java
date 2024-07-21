31c819c9dc78860d31a06c1750a6472653560b21
hajeu <s22072@gsm.hs.kr>
2024-07-21 11:45:14 +0900

    refactor(oneseo): 실물 원서 제출 여부 반환

--- src/main/java/team/themoment/hellogsmv3/domain/oneseo/service/ModifyRealOneseoArrivedYnService.java
+++ src/main/java/team/themoment/hellogsmv3/domain/oneseo/service/ModifyRealOneseoArrivedYnService.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import team.themoment.hellogsmv3.domain.member.entity.Member;
 import team.themoment.hellogsmv3.domain.member.service.MemberService;
+import team.themoment.hellogsmv3.domain.oneseo.dto.response.ArrivedStatusResDto;
 import team.themoment.hellogsmv3.domain.oneseo.entity.Oneseo;
 import team.themoment.hellogsmv3.domain.oneseo.entity.type.YesNo;
 import team.themoment.hellogsmv3.domain.oneseo.repository.OneseoRepository;
@@ -19,13 +20,15 @@ public class ModifyRealOneseoArrivedYnService {
     private final OneseoRepository oneseoRepository;
 
     @Transactional
-    public void execute(Long memberId) {
+    public ArrivedStatusResDto execute(Long memberId) {
         Member member = memberService.findByIdOrThrow(memberId);
 
         Oneseo oneseo = oneseoRepository.findByMember(member)
                 .orElseThrow(() -> new ExpectedException("해당 지원자의 원서를 찾을 수 없습니다. member ID: " + memberId, HttpStatus.NOT_FOUND));
 
-        oneseoRepository.save(switchRealOneseoArrivedYn(oneseo));
+        Oneseo modifiedOneseo = oneseoRepository.save(switchRealOneseoArrivedYn(oneseo));
+
+        return new ArrivedStatusResDto(modifiedOneseo.getRealOneseoArrivedYn());
     }
 
     private Oneseo switchRealOneseoArrivedYn(Oneseo oneseo) {
