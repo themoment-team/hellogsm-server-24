4308450e844269d668cef3102d0288c813543755
hajeu <s22072@gsm.hs.kr>
2024-07-24 00:09:34 +0900

    refactor(oneseo): YesNo enum static import로 변경 및 ==연산자를 .epuals()메서드로 변경

--- src/main/java/team/themoment/hellogsmv3/domain/oneseo/service/DeleteOneseoService.java
+++ src/main/java/team/themoment/hellogsmv3/domain/oneseo/service/DeleteOneseoService.java
@@ -7,9 +7,10 @@ import org.springframework.stereotype.Service;
 import team.themoment.hellogsmv3.domain.member.entity.Member;
 import team.themoment.hellogsmv3.domain.member.service.MemberService;
 import team.themoment.hellogsmv3.domain.oneseo.entity.Oneseo;
-import team.themoment.hellogsmv3.domain.oneseo.entity.type.YesNo;
 import team.themoment.hellogsmv3.domain.oneseo.repository.OneseoRepository;
 import team.themoment.hellogsmv3.global.exception.error.ExpectedException;
+
+import static team.themoment.hellogsmv3.domain.oneseo.entity.type.YesNo.YES;
 
 @Service
 @RequiredArgsConstructor
@@ -24,7 +25,7 @@ public class DeleteOneseoService {
         Oneseo oneseo = oneseoRepository.findByMember(member)
                 .orElseThrow(() -> new ExpectedException("해당 지원자의 원서를 찾을 수 없습니다. member ID: " + memberId, HttpStatus.NOT_FOUND));
 
-        if (oneseo.getFinalSubmittedYn() == YesNo.YES) {
+        if (oneseo.getFinalSubmittedYn().equals(YES)) {
             throw new ExpectedException("최종제출을 완료한 원서입니다.", HttpStatus.BAD_REQUEST);
         }
 
