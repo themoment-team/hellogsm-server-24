de5354882004f2139488d4267478e608fe71bfa9
jangwooooo <s22018@gsm.hs.kr>
2024-07-24 01:44:50 +0900

    test(member): CommonCodeService 클래스의 validateAndDelete 메소드는 인증 코드가 존재하지 않으면 ExpectedException을 던진다 case 테스트코드 작성

--- src/test/java/team/themoment/hellogsmv3/domain/member/service/CommonCodeServiceTest.java
+++ src/test/java/team/themoment/hellogsmv3/domain/member/service/CommonCodeServiceTest.java
@@ -7,12 +7,17 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
 import team.themoment.hellogsmv3.domain.member.entity.AuthenticationCode;
 import team.themoment.hellogsmv3.domain.member.repo.CodeRepository;
+import team.themoment.hellogsmv3.global.exception.error.ExpectedException;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
@@ -65,6 +70,27 @@ class CommonCodeServiceTest {
                 commonCodeService.validateAndDelete(memberId, validCode, validPhoneNumber);
 
                 verify(codeRepository).delete(authenticationCode);
+            }
+        }
+
+        @Nested
+        @DisplayName("인증 코드가 존재하지 않으면")
+        class Context_with_non_existing_code {
+
+            @BeforeEach
+            void setUp() {
+                given(codeRepository.findByMemberId(anyLong())).willReturn(Optional.empty());
+            }
+
+            @Test
+            @DisplayName("ExpectedException을 던진다")
+            void it_throws_expected_exception() {
+                ExpectedException exception = assertThrows(ExpectedException.class, () -> {
+                    commonCodeService.validateAndDelete(memberId, validCode, validPhoneNumber);
+                });
+
+                assertEquals("사용자의 code가 존재하지 않습니다. 사용자의 ID : " + memberId, exception.getMessage());
+                assertEquals(HttpStatus.BAD_REQUEST, exception.getStatusCode());
             }
         }
     }
