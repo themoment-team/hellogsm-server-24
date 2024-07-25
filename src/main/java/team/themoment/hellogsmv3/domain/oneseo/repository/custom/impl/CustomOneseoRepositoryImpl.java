04e07fd198e2fe2f2ae87c9d92b336a6cd076531
hajeu <s22072@gsm.hs.kr>
2024-07-25 22:57:33 +0900

    refactor(oneseo): 중복된 클래스 제거

--- src/main/java/team/themoment/hellogsmv3/domain/oneseo/repository/custom/impl/CustomOneseoRepositoryImpl.java
+++ src/main/java/team/themoment/hellogsmv3/domain/oneseo/repository/custom/impl/CustomOneseoRepositoryImpl.java
@@ -1,13 +1,24 @@
 package team.themoment.hellogsmv3.domain.oneseo.repository.custom.impl;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
+import team.themoment.hellogsmv3.domain.application.type.ScreeningCategory;
+import team.themoment.hellogsmv3.domain.oneseo.dto.request.TestResultTag;
 import team.themoment.hellogsmv3.domain.oneseo.dto.response.AdmissionTicketsResDto;
+import team.themoment.hellogsmv3.domain.oneseo.entity.Oneseo;
+import team.themoment.hellogsmv3.domain.oneseo.entity.type.Screening;
+import team.themoment.hellogsmv3.domain.oneseo.entity.type.YesNo;
 import team.themoment.hellogsmv3.domain.oneseo.repository.custom.CustomOneseoRepository;
 
+import static com.querydsl.core.types.ExpressionUtils.anyOf;
 import static team.themoment.hellogsmv3.domain.member.entity.QMember.member;
+import static team.themoment.hellogsmv3.domain.oneseo.entity.QEntranceTestResult.entranceTestResult;
 import static team.themoment.hellogsmv3.domain.oneseo.entity.QOneseo.oneseo;
 import static team.themoment.hellogsmv3.domain.oneseo.entity.QOneseoPrivacyDetail.oneseoPrivacyDetail;
 import static team.themoment.hellogsmv3.domain.oneseo.entity.type.YesNo.*;
@@ -37,5 +48,134 @@ public class CustomOneseoRepositoryImpl implements CustomOneseoRepository {
                 .join(oneseo.member, member)
                 .where(oneseo.finalSubmittedYn.eq(YES))
                 .fetch();
+    }
+
+    @Override
+    public Page<Oneseo> findAllByKeywordAndScreeningAndSubmissionStatusAndTestResult(
+            String keyword,
+            ScreeningCategory screeningTag,
+            YesNo isSubmitted,
+            TestResultTag testResultTag,
+            Pageable pageable
+    ) {
+
+        BooleanBuilder builder = createBooleanBuilder(
+                keyword,
+                screeningTag,
+                isSubmitted,
+                testResultTag
+        );
+
+        List<Oneseo> oneseos = queryFactory.selectFrom(oneseo)
+                .leftJoin(oneseo.member, member).fetchJoin()
+                .leftJoin(oneseoPrivacyDetail).on(oneseoPrivacyDetail.oneseo.eq(oneseo))
+                .leftJoin(entranceTestResult).on(entranceTestResult.id.eq(oneseo.id))
+                .where(builder)
+                .orderBy(oneseo.oneseoSubmitCode.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        return new PageImpl<>(
+                oneseos,
+                pageable,
+                oneseos.size()
+        );
+    }
+
+    private BooleanBuilder createBooleanBuilder(
+            String keyword,
+            ScreeningCategory screeningTag,
+            YesNo isSubmitted,
+            TestResultTag testResultTag
+    ) {
+
+        BooleanBuilder builder = new BooleanBuilder();
+        builder.and(oneseo.finalSubmittedYn.eq(YesNo.YES));
+        applyKeyword(builder, keyword);
+        applyScreeningTag(builder, screeningTag);
+        applyIsSubmittedTag(builder, isSubmitted);
+        applyTestResultTag(builder, testResultTag);
+
+        return builder;
+    }
+
+    private void applyKeyword(
+            BooleanBuilder builder,
+            String keyword
+    ) {
+        if (keyword == null) return;
+
+        builder.and(
+                anyOf(
+                        oneseo.member.name.like("%" + keyword + "%"),
+                        oneseoPrivacyDetail.schoolName.like("%" + keyword + "%"),
+                        oneseo.member.phoneNumber.like("%" + keyword + "%")
+                )
+        );
+    }
+
+    private void applyScreeningTag(
+            BooleanBuilder builder,
+            ScreeningCategory screeningTag
+    ) {
+        if (screeningTag == null) return;
+
+        switch (screeningTag) {
+            case GENERAL ->
+                    builder.and(
+                            oneseo.appliedScreening.eq(Screening.GENERAL)
+                    );
+            case SPECIAL ->
+                    builder.and(
+                            oneseo.appliedScreening.eq(Screening.SPECIAL)
+                    );
+            case EXTRA ->
+                    builder.andAnyOf(
+                            oneseo.appliedScreening.eq(Screening.EXTRA_ADMISSION),
+                            oneseo.appliedScreening.eq(Screening.EXTRA_VETERANS)
+                    );
+        }
+    }
+
+    private void applyIsSubmittedTag(
+            BooleanBuilder builder,
+            YesNo isSubmitted
+    ) {
+        if (isSubmitted == null) return;
+
+
+        switch (isSubmitted) {
+            case YES ->
+                    builder.and(
+                            oneseo.realOneseoArrivedYn.eq(YesNo.YES)
+                    );
+            case NO ->
+                    builder.and(
+                            oneseo.realOneseoArrivedYn.eq(YesNo.NO)
+                    );
+        }
+    }
+
+    private void applyTestResultTag(
+            BooleanBuilder builder,
+            TestResultTag testResultTag
+    ) {
+
+        switch (testResultTag) {
+            case FIRST_PASS ->
+                    builder.and(
+                            entranceTestResult.firstTestPassYn.eq(YesNo.YES)
+                    );
+            case FINAL_PASS ->
+                    builder.and(
+                            entranceTestResult.secondTestPassYn.eq(YesNo.YES)
+                    );
+            case FALL ->
+                    builder.andAnyOf(
+                            entranceTestResult.firstTestPassYn.eq(YesNo.NO),
+                            entranceTestResult.firstTestPassYn.eq(YesNo.NO)
+                    );
+        }
     }
 }
