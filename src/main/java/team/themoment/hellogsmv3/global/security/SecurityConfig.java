dffefab63c6a0f5399e753f7910cf86cb4407c57
신희성 <vvwv3092@gmail.com>
2024-07-26 14:37:51 +0900

    add(oneseo): 심층면접 점수 기입 api 시큐리티 권한 매핑 추가

--- src/main/java/team/themoment/hellogsmv3/global/security/SecurityConfig.java
+++ src/main/java/team/themoment/hellogsmv3/global/security/SecurityConfig.java
@@ -228,6 +228,9 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.PATCH, "/oneseo/v3/aptitude-score/{memberId}").hasAnyAuthority(
                         Role.ADMIN.name()
                 )
+                .requestMatchers(HttpMethod.PATCH, "/oneseo/v3/interview-score/{memberId}").hasAnyAuthority(
+                        Role.ADMIN.name()
+                )
                 .requestMatchers(HttpMethod.POST, "/oneseo/v3/image").hasAnyAuthority(
                         Role.APPLICANT.name(),
                         Role.ADMIN.name(),
