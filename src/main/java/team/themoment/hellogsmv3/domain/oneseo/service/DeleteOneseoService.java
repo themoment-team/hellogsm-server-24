6d788bcc0b51ec975bb43a95c04486eb431b03c7
신희성 <vvwv3092@gmail.com>
2024-07-28 02:21:38 +0900

    Revert "fix(oneseo): 원서 삭제시 연관된 원서 엔티티 삭제 로직 추가"
    This reverts commit 65fdda898279f246fd5ae9f829925dbd55a3d853.

--- src/main/java/team/themoment/hellogsmv3/domain/oneseo/service/DeleteOneseoService.java
+++ src/main/java/team/themoment/hellogsmv3/domain/oneseo/service/DeleteOneseoService.java
@@ -6,11 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import team.themoment.hellogsmv3.domain.member.entity.Member;
 import team.themoment.hellogsmv3.domain.member.service.MemberService;
-import team.themoment.hellogsmv3.domain.oneseo.entity.MiddleSchoolAchievement;
 import team.themoment.hellogsmv3.domain.oneseo.entity.Oneseo;
-import team.themoment.hellogsmv3.domain.oneseo.entity.OneseoPrivacyDetail;
-import team.themoment.hellogsmv3.domain.oneseo.repository.MiddleSchoolAchievementRepository;
-import team.themoment.hellogsmv3.domain.oneseo.repository.OneseoPrivacyDetailRepository;
 import team.themoment.hellogsmv3.domain.oneseo.repository.OneseoRepository;
 import team.themoment.hellogsmv3.global.exception.error.ExpectedException;
 
@@ -21,10 +17,8 @@ import static team.themoment.hellogsmv3.domain.oneseo.entity.type.YesNo.YES;
 public class DeleteOneseoService {
 
     private final MemberService memberService;
-    private final OneseoService oneseoService;
     private final OneseoRepository oneseoRepository;
-    private final OneseoPrivacyDetailRepository oneseoPrivacyDetailRepository;
-    private final MiddleSchoolAchievementRepository middleSchoolAchievementRepository;
+    private final OneseoService oneseoService;
 
     @Transactional
     public void execute(Long memberId) {
@@ -35,11 +29,6 @@ public class DeleteOneseoService {
             throw new ExpectedException("최종제출을 완료한 원서입니다.", HttpStatus.BAD_REQUEST);
         }
 
-        OneseoPrivacyDetail oneseoPrivacyDetail = oneseoPrivacyDetailRepository.findByOneseo(oneseo);
-        MiddleSchoolAchievement middleSchoolAchievement = middleSchoolAchievementRepository.findByOneseo(oneseo);
-
-        oneseoPrivacyDetailRepository.delete(oneseoPrivacyDetail);
-        middleSchoolAchievementRepository.delete(middleSchoolAchievement);
         oneseoRepository.delete(oneseo);
     }
 }
