47c782d7a82c23e2311c72e9a09185558d43eaa1
신희성 <vvwv3092@gmail.com>
2024-07-31 18:00:33 +0900

    add(global): swagger 설정 메시지 컨버터 요류

---
+++ src/main/java/team/themoment/hellogsmv3/global/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package team.themoment.hellogsmv3.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+//    @Bean
+//    public GroupedOpenApi api() {
+//        return GroupedOpenApi.builder()
+//                .group("ONESEO")
+//                .pathsToMatch("/oneseo/**")
+//                .packagesToScan("team.themoment.hellogsmv3.domain.oneseo.controller")
+//                .build();
+//    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Hello, GSM 2024")
+                .description("광주소프트웨어마이스터고등학교 입학지원 시스템");
+    }
+}
--- src/main/java/team/themoment/hellogsmv3/global/config/WebArgumentResolverConfig.java
+++ src/main/java/team/themoment/hellogsmv3/global/config/WebArgumentResolverConfig.java
@@ -1,6 +1,8 @@
 package team.themoment.hellogsmv3.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import team.themoment.hellogsmv3.global.common.handler.AuthenticationArgumentResolver;
@@ -12,5 +14,10 @@ public class WebArgumentResolverConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new AuthenticationArgumentResolver());
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(0, new MappingJackson2HttpMessageConverter());
     }
 }
