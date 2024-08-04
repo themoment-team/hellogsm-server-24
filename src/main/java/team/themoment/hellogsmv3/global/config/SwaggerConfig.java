d54fa9533500310b75be985d415ce4d5e2f9f28d
신희성 <vvwv3092@gmail.com>
2024-08-04 18:58:33 +0900

    update(global): swagger config 수정

--- src/main/java/team/themoment/hellogsmv3/global/config/SwaggerConfig.java
+++ src/main/java/team/themoment/hellogsmv3/global/config/SwaggerConfig.java
@@ -1,25 +1,22 @@
 package team.themoment.hellogsmv3.global.config;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@OpenAPIDefinition(
+        info = @Info(title = "Hello, GSM 2024",
+                description = "광주소프트웨어마이스터고등학교 입학지원 시스템",
+                version = "v1"))
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
-    }
-
-    private Info apiInfo() {
-        return new Info()
-                .title("Hello, GSM 2024")
-                .description("광주소프트웨어마이스터고등학교 입학지원 시스템")
-                .version("1.0");
+    public GroupedOpenApi api() {
+        return GroupedOpenApi.builder()
+                .group("Hello, GSM 2024 API")
+                .pathsToMatch("/member/**", "/oneseo/**")
+                .build();
     }
 }
