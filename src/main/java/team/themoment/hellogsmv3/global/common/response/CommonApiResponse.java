46e018eba429e6d4a75bf7087df0145470772ce4
신희성 <vvwv3092@gmail.com>
2024-08-04 19:42:43 +0900

    add(global): 공통 response 객체 Schema 추가

--- src/main/java/team/themoment/hellogsmv3/global/common/response/CommonApiResponse.java
+++ src/main/java/team/themoment/hellogsmv3/global/common/response/CommonApiResponse.java
@@ -2,13 +2,18 @@ package team.themoment.hellogsmv3.global.common.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.annotation.Nonnull;
 import org.springframework.http.HttpStatus;
 
 public record CommonApiResponse<T>(
+        @Schema(description = "상태 메시지", nullable = false, example = "OK")
         HttpStatus status,
+        @Schema(description = "상태 코드", nullable = false, example = "200")
         int code,
+        @Schema(description = "메시지", nullable = false, example = "완료되었습니다.")
         String message,
+        @Schema(description = "데이터", nullable = true)
         @JsonInclude(Include.NON_NULL)
         T data
 ) {
