4f8796ac135d5851d5e99ea9277e34140eb41917
신희성 <vvwv3092@gmail.com>
2024-08-11 16:58:17 +0900

    cd(global): deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# 배포 그룹 이름을 확인합니다. (CodeDeploy 환경 변수를 사용)
-DEPLOYMENT_GROUP_NAME=$(echo $DEPLOYMENT_GROUP_NAME)
-
 # 배포 그룹에 따라 다른 작업을 수행합니다.
 if [ "$DEPLOYMENT_GROUP_NAME" == "hello-stage" ]; then
   chmod +x /home/ec2-user/builds/scripts/dev-deploy.sh
