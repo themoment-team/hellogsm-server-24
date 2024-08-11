9ec2e41cd7b63b04a5f2385106842a4e1e188463
신희성 <vvwv3092@gmail.com>
2024-08-11 17:05:49 +0900

    cd(global): deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ DG_NAME=$(echo $DEPLOYMENT_GROUP_NAME)
 # 배포 그룹에 따라 다른 배포 스크립트를 실행합니다.
 if [ "$DG_NAME" == "hello-stage" ]; then
   chmod +x /home/ec2-user/builds/scripts/dev-deploy.sh
-  /home/ec2-user/builds/scripts/stage-deploy.sh
+  /home/ec2-user/builds/scripts/dev-deploy.sh
 
 elif [ "$DG_NAME" == "hello-prod" ]; then
   chmod +x /home/ec2-user/builds/scripts/prod-deploy.sh
