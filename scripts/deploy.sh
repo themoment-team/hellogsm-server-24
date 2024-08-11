d7ee813dd4f274558a288b98512419b211362aaa
신희성 <vvwv3092@gmail.com>
2024-08-11 17:04:35 +0900

    cd(global): deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# debug
-set -x
-
 # CodeDeploy의 환경 변수를 확인합니다.
 DG_NAME=$(echo $DEPLOYMENT_GROUP_NAME)
 
 # 배포 그룹에 따라 다른 배포 스크립트를 실행합니다.
 if [ "$DG_NAME" == "hello-stage" ]; then
-  chmod +x /home/ec2-user/builds/scripts/stage-deploy.sh
+  chmod +x /home/ec2-user/builds/scripts/dev-deploy.sh
   /home/ec2-user/builds/scripts/stage-deploy.sh
 
 elif [ "$DG_NAME" == "hello-prod" ]; then
