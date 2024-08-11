cdfea48e9fa39e87cdf0fe7c796cb9460833119b
신희성 <vvwv3092@gmail.com>
2024-08-11 17:02:41 +0900

    cd(global): deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-# 배포 그룹에 따라 다른 작업을 수행합니다.
-if [ "$DEPLOYMENT_GROUP_NAME" == "hello-stage" ]; then
-  chmod +x /home/ec2-user/builds/scripts/dev-deploy.sh
-  /home/ec2-user/builds/scripts/dev-deploy.sh
+# debug
+set -x
 
-elif [ "$DEPLOYMENT_GROUP_NAME" == "hello-prod" ]; then
+# CodeDeploy의 환경 변수를 확인합니다.
+DG_NAME=$(echo $DEPLOYMENT_GROUP_NAME)
+
+# 배포 그룹에 따라 다른 배포 스크립트를 실행합니다.
+if [ "$DG_NAME" == "hello-stage" ]; then
+  chmod +x /home/ec2-user/builds/scripts/stage-deploy.sh
+  /home/ec2-user/builds/scripts/stage-deploy.sh
+
+elif [ "$DG_NAME" == "hello-prod" ]; then
   chmod +x /home/ec2-user/builds/scripts/prod-deploy.sh
   /home/ec2-user/builds/scripts/prod-deploy.sh
 
+else
+  echo "알 수 없는 배포 그룹입니다: $DG_NAME"
+  exit 1
 fi
