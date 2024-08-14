ae34c2db8722cff7c725ba1d98449ebbb65b529b
신희성 <vvwv3092@gmail.com>
2024-08-14 13:56:38 +0900

    update(global): TimeBasedFilter에 사용되는 변수명 알아보고 쉽게 변경

--- src/main/java/team/themoment/hellogsmv3/global/security/filter/TimeBasedFilter.java
+++ src/main/java/team/themoment/hellogsmv3/global/security/filter/TimeBasedFilter.java
@@ -21,24 +21,16 @@ import java.util.Map;
 public class TimeBasedFilter extends OncePerRequestFilter {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
-    private final Map<String, TimeRange> urlTimeRanges = new HashMap<>();
+    private final Map<String, RequestPeriod> urlRequestPeriods = new HashMap<>();
 
-    private static class TimeRange {
-        private final LocalDateTime startTime;
-        private final LocalDateTime endTime;
-
-        public TimeRange(LocalDateTime startTime, LocalDateTime endTime) {
-            this.startTime = startTime;
-            this.endTime = endTime;
-        }
-
+    private record RequestPeriod(LocalDateTime startTime, LocalDateTime endTime) {
         public boolean isWithinRange(LocalDateTime currentTime) {
             return currentTime.isAfter(startTime) && currentTime.isBefore(endTime);
         }
     }
 
     public TimeBasedFilter addFilter(HttpMethod httpMethod, String uri, LocalDateTime startTime, LocalDateTime endTime) {
-        urlTimeRanges.put(uri + ":" + httpMethod, new TimeRange(startTime, endTime));
+        urlRequestPeriods.put(uri + ":" + httpMethod, new RequestPeriod(startTime, endTime));
         return this;
     }
 
@@ -55,16 +47,16 @@ public class TimeBasedFilter extends OncePerRequestFilter {
         }
 
         LocalDateTime currentTime = LocalDateTime.now();
-        String timeRangeKey = requestUri + ":" + requestMethod;
+        String requestPeriodKey = requestUri + ":" + requestMethod;
 
-        if (urlTimeRanges.containsKey(timeRangeKey)) {
-            TimeRange timeRange = urlTimeRanges.get(timeRangeKey);
+        if (urlRequestPeriods.containsKey(requestPeriodKey)) {
+            RequestPeriod requestPeriod = urlRequestPeriods.get(requestPeriodKey);
 
-            if (timeRange.isWithinRange(currentTime)) {
+            if (requestPeriod.isWithinRange(currentTime)) {
                 filterChain.doFilter(request, response);
             } else {
                 String message = String.format("%s 요청이 거부되었습니다. " +
-                        "현재 시간: %s, 해당 요청은 %s ~ %s 이내에만 처리 가능합니다.", timeRangeKey, currentTime, timeRange.startTime, timeRange.endTime);
+                        "현재 시간: %s, 해당 요청은 %s ~ %s 이내에만 처리 가능합니다.", requestPeriodKey, currentTime, requestPeriod.startTime, requestPeriod.endTime);
                 log.warn(message);
                 sendErrorResponse(response, message);
             }
