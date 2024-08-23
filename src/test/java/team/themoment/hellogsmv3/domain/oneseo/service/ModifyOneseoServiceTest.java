7440f12ad031a42cfc72722a28b5f431ca70070f
신희성 <vvwv3092@gmail.com>
2024-08-23 14:36:46 +0900

    test(oneseo): 원서 수정 관련 테스트코드 수정

--- src/test/java/team/themoment/hellogsmv3/domain/oneseo/service/ModifyOneseoServiceTest.java
+++ src/test/java/team/themoment/hellogsmv3/domain/oneseo/service/ModifyOneseoServiceTest.java
@@ -15,6 +15,7 @@ import team.themoment.hellogsmv3.domain.oneseo.entity.MiddleSchoolAchievement;
 import team.themoment.hellogsmv3.domain.oneseo.entity.Oneseo;
 import team.themoment.hellogsmv3.domain.oneseo.entity.OneseoPrivacyDetail;
 import team.themoment.hellogsmv3.domain.oneseo.entity.ScreeningChangeHistory;
+import team.themoment.hellogsmv3.domain.oneseo.entity.type.DesiredMajors;
 import team.themoment.hellogsmv3.domain.oneseo.entity.type.GraduationType;
 import team.themoment.hellogsmv3.domain.oneseo.entity.type.Major;
 import team.themoment.hellogsmv3.domain.oneseo.entity.type.Screening;
@@ -116,6 +117,11 @@ class ModifyOneseoServiceTest {
         String schoolName = "금호중앙중학교";
         String schoolAddress = "광주 어딘가";
         Screening screening = GENERAL;
+        DesiredMajors desiredMajors = DesiredMajors.builder()
+                .firstDesiredMajor(firstDesiredMajor)
+                .secondDesiredMajor(secondDesiredMajor)
+                .thirdDesiredMajor(thirdDesiredMajor)
+                .build();
 
         OneseoReqDto oneseoReqDto = new OneseoReqDto(
                 guardianName,
@@ -146,6 +152,7 @@ class ModifyOneseoServiceTest {
 
                 Oneseo oneseo = Oneseo.builder()
                         .id(1L)
+                        .desiredMajors(desiredMajors)
                         .build();
 
                 OneseoPrivacyDetail oneseoPrivacyDetail = OneseoPrivacyDetail.builder()
@@ -226,23 +233,27 @@ class ModifyOneseoServiceTest {
                 Screening beforeScreening = SPECIAL;
                 Screening afterScreening = GENERAL;
 
+                Oneseo oneseo = Oneseo.builder()
+                        .id(1L)
+                        .appliedScreening(beforeScreening)
+                        .desiredMajors(desiredMajors)
+                        .build();
+
                 Member existingMember = mock(Member.class);
-                Oneseo existingOneseo = mock(Oneseo.class);
                 OneseoPrivacyDetail existingPrivacyDetail = mock(OneseoPrivacyDetail.class);
                 MiddleSchoolAchievement existingAchievement = mock(MiddleSchoolAchievement.class);
 
                 given(memberService.findByIdOrThrow(memberId)).willReturn(existingMember);
-                given(oneseoService.findByMemberOrThrow(existingMember)).willReturn(existingOneseo);
-                given(oneseoPrivacyDetailRepository.findByOneseo(existingOneseo)).willReturn(existingPrivacyDetail);
-                given(middleSchoolAchievementRepository.findByOneseo(existingOneseo)).willReturn(existingAchievement);
-                given(existingOneseo.getAppliedScreening()).willReturn(beforeScreening);
+                given(oneseoService.findByMemberOrThrow(existingMember)).willReturn(oneseo);
+                given(oneseoPrivacyDetailRepository.findByOneseo(oneseo)).willReturn(existingPrivacyDetail);
+                given(middleSchoolAchievementRepository.findByOneseo(oneseo)).willReturn(existingAchievement);
                 given(existingPrivacyDetail.getGraduationType()).willReturn(GED);
 
                 modifyOneseoService.execute(oneseoReqDto, memberId);
                 ArgumentCaptor<ScreeningChangeHistory> screeningChangeHistoryArgumentCaptor = ArgumentCaptor.forClass(ScreeningChangeHistory.class);
 
                 verify(screeningChangeHistoryRepository).save(screeningChangeHistoryArgumentCaptor.capture());
-                verify(calculateGedService).execute(any(MiddleSchoolAchievementReqDto.class), eq(existingOneseo), eq(GED));
+                verify(calculateGedService).execute(any(MiddleSchoolAchievementReqDto.class), eq(oneseo), eq(GED));
 
                 ScreeningChangeHistory capturedScreeningChangeHistory = screeningChangeHistoryArgumentCaptor.getValue();
 
