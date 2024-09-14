35cb0b08f20e1fbb601faf459c024c3838233b7b
신희성 <vvwv3092@gmail.com>
2024-09-15 01:13:26 +0900

    fix(date): date API swagger 설정 추가

--- src/main/java/team/themoment/hellogsmv3/domain/common/date/controller/DateController.java
+++ src/main/java/team/themoment/hellogsmv3/domain/common/date/controller/DateController.java
@@ -1,12 +1,14 @@
 package team.themoment.hellogsmv3.domain.common.date.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import team.themoment.hellogsmv3.domain.common.date.dto.DateResDto;
 import team.themoment.hellogsmv3.domain.common.date.service.QueryDateService;
 
+@Tag(name = "Date API", description = "일정 관련 API입니다.")
 @RestController
 @RequiredArgsConstructor
 public class DateController {
--- src/main/java/team/themoment/hellogsmv3/global/config/SwaggerConfig.java
+++ src/main/java/team/themoment/hellogsmv3/global/config/SwaggerConfig.java
@@ -32,7 +32,7 @@ public class SwaggerConfig {
     public GroupedOpenApi api(OperationCustomizer operationCustomizer) {
         return GroupedOpenApi.builder()
                 .group("Hello, GSM 2024 API")
-                .pathsToMatch("/member/**", "/oneseo/**", "/auth/**")
+                .pathsToMatch("/member/**", "/oneseo/**", "/auth/**", "/date")
                 .addOperationCustomizer(operationCustomizer)
                 .build();
     }
