d285aa612baa09021829b4aa97ccf5644e92ab2c
신희성 <vvwv3092@gmail.com>
2024-09-27 13:52:03 +0900

    fix(member): 전형 결좌 조회 api 스펙에 배정 학과 추가

--- src/main/java/team/themoment/hellogsmv3/domain/member/dto/response/FoundMemberTestResDto.java
+++ src/main/java/team/themoment/hellogsmv3/domain/member/dto/response/FoundMemberTestResDto.java
@@ -1,11 +1,13 @@
 package team.themoment.hellogsmv3.domain.member.dto.response;
 
 import lombok.Builder;
+import team.themoment.hellogsmv3.domain.oneseo.entity.type.Major;
 import team.themoment.hellogsmv3.domain.oneseo.entity.type.YesNo;
 
 @Builder
 public record FoundMemberTestResDto(
         YesNo firstTestPassYn,
-        YesNo secondTestPassYn
+        YesNo secondTestPassYn,
+        Major decidedMajor
 ) {
 }
