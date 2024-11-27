e1e2a4ba80fbbabc7f7a0ff55c9d716fb3c189e2
jangwooooo <s22018@gsm.hs.kr>
2024-11-27 16:53:05 +0900

    add: 비동기 예외처리 GlobalAsyncExceptionHandler 추가

--- src/main/java/team/themoment/hellogsmv3/global/config/AsyncConfig.java
+++ src/main/java/team/themoment/hellogsmv3/global/config/AsyncConfig.java
@@ -1,16 +1,22 @@
 package team.themoment.hellogsmv3.global.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import team.themoment.hellogsmv3.global.exception.GlobalAsyncExceptionHandler;
 
 import java.util.concurrent.Executor;
 
 @EnableAsync
 @Configuration
+@RequiredArgsConstructor
 public class AsyncConfig implements AsyncConfigurer {
+
+    private final GlobalAsyncExceptionHandler globalAsyncExceptionHandler;
 
     @Override
     @Bean(name = "discordTaskExecutor")
@@ -19,5 +25,10 @@ public class AsyncConfig implements AsyncConfigurer {
         executor.setThreadNamePrefix("Discord-Task");
         executor.initialize();
         return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler(){
+        return globalAsyncExceptionHandler;
     }
 }
