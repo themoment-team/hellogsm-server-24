5e166e81957ac10f35b6d1623d46fd37fd98218c
wwwcomcomcomcom <iieiiergn@gmail.com>
2025-01-16 17:25:16 +0900

    fix: 컨테이너가 실행중이지 않을경우 발생하는 에러를 해결했습니다.
    docker ps는 실행중인 컨테이너만 반환하므로, 이전버전의 컨테이너가 모종의 이유로 실행중이지 않다면 배포중 에러가 발생했습니다. -a 파라미터로 실행중이지 않은 컨테이너까지 검색하도록 하였습니다.

--- scripts/prod-deploy.sh
+++ scripts/prod-deploy.sh
@@ -5,15 +5,23 @@ CONTAINER_NAME=hellogsm-prod-server
 DOCKERFILE_NAME=DockerfileProd
 
 echo "> 현재 실행 중인 Docker 컨테이너 ID 확인" >> /home/ec2-user/deploy.log
-CURRENT_CONTAINER_ID=$(docker ps -q -f name=$CONTAINER_NAME)
+RUNNING_CONTAINER_ID=$(docker ps -q -f name=$CONTAINER_NAME)
+EXISTING_CONTAINER_ID=$(docker ps -a -q -f name=$CONTAINER_NAME)
 
-if [ -z $CURRENT_CONTAINER_ID ]
+if [ -z $RUNNING_CONTAINER_ID ]
 then
   echo "> 현재 구동중인 Docker 컨테이너가 없으므로 종료하지 않습니다." >> /home/ec2-user/deploy.log
 else
-  echo "> sudo docker stop $CURRENT_CONTAINER_ID"
-  sudo docker stop $CURRENT_CONTAINER_ID
-  sudo docker rm $CURRENT_CONTAINER_ID
+  echo "> sudo docker stop $RUNNING_CONTAINER_ID"
+  sudo docker stop $RUNNING_CONTAINER_ID
+fi
+
+if [ -z $EXISTING_CONTAINER_ID ]
+then
+  echo "> 현재 존재하는 Docker 컨테이너가 없으므로 삭제하지 않습니다." >> /home/ec2-user/deploy.log
+else
+  echo "> sudo docker rm $EXISTING_CONTAINER_ID"
+  sudo docker rm $EXISTING_CONTAINER_ID
 fi
 
 cd /home/ec2-user/builds/
